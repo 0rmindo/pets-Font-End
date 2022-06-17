0afcaf964b6e39eccdcd25db284b7310e66432bf
Ormindo <pcos1@aluno.ifal.edu.br>
2022-06-17 17:24:50 -0300

    Semana Mult-Stack#3 - Aula 5 - 20min
    Feito até 20 minutos da aula 5 Semana Mult-Stak#3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,12 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material';
 import tema from '../ui/theme/tema';
 import Cabecalho from '../ui/components/Cabecalho/Cabecalho'
+import CabecalhoAdmin from '../ui/components/CabecalhoAdmin/CabecalhoAdmin'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={tema}>
-      <Cabecalho />
+      <CabecalhoAdmin />
       <Component {...pageProps} />
     </ThemeProvider>
   )
---
+++ src/ui/components/CabecalhoAdmin/CabecalhoAdmin.style.tsx
@@ -0,0 +1,27 @@
+import { styled } from "@mui/material";
+
+export const CabecalhoContainer = styled('header')`
+    height: 115px;
+    background-color: #f6f6f6;
+    padding: ${({ theme }) => theme.spacing(2) };
+
+    div {
+        height: 100%;
+        max-width: 970px;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: ${({ theme }) => theme.spacing(2) };
+
+    }
+`;
+
+export const Logo = styled('img')``;
+
+export const LinksContainer = styled('nav')`
+    display: flex;
+    gap: ${({ theme }) => theme.spacing(2) };
+    flex-wrap: wrap;
+    justify-content: flex-end;
+`;
